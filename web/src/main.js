32a3930447998af539b08686e70efe926bf751cd
Yan Fan <theyanfan@gmail.com>
2020-05-16 09:02:11 +0900

    Added i18n

--- web/src/main.js
+++ web/src/main.js
@@ -4,11 +4,13 @@ import router from './router';
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import '@mdi/font/css/materialdesignicons.css';
+import i18n from './i18n';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
--- web/vue.config.js
+++ web/vue.config.js
@@ -2,4 +2,13 @@ module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true,
+    },
+  },
 };
