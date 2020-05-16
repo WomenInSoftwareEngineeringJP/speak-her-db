9230a4d6cb1a182ba8cbfbbba8fbd18707b7b51e
Yan Fan <theyanfan@gmail.com>
2020-05-16 09:40:08 +0900

    Add Nav

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,56 +1,25 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
+      <Nav/>
       <HelloWorld/>
+      <HelloI18n/>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import HelloI18n from './components/HelloI18n.vue';
+import Nav from './components/Nav.vue';
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    HelloI18n,
+    Nav,
   },
 
   data: () => ({
---
+++ web/src/components/Nav.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="nav">
+    <img alt="Vue logo" src="../assets/logo.png" />
+
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </div>
+
+</template>
+
+<style>
+#nav {
+  display: flex;
+  align-items: center;
+  text-align: left;
+  padding: 1rem;
+  color: #42b983;
+  background-color: #3d536a;
+}
+#nav img {
+  margin-right: 1rem;
+  max-height: 1rem;
+}
+#nav a {
+  margin-right: 1.5rem;
+  font-weight: bold;
+  color: #fff;
+  text-decoration: none;
+}
+</style>
+
+<script>
+export default {
+  name: 'Nav',
+};
+</script>
