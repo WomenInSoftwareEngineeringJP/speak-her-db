40c00a4569c4dad1b58924d21da6a95f4cfbf8c1
Yan Fan <theyanfan@gmail.com>
2020-05-16 10:16:29 +0900

    Locale manually set, got rid of <i18n>

--- web/src/components/HelloI18n.vue
+++ web/src/components/HelloI18n.vue
@@ -3,15 +3,16 @@
 </template>
 
 <script>
+import i18n from '@/i18n';
+
+console.log('Active locale: ', i18n.locale);
+
 export default {
   name: 'HelloI18n',
+  methods: {
+    setLocale(locale) {
+      this.$i18n.locale = locale;
+    },
+  },
 };
 </script>
-
-<i18n>
-{
-  "en": {
-    "hello": "Hello i18n in SFC!"
-  }
-}
-</i18n>
