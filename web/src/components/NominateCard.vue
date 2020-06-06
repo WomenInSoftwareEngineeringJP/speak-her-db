f61723456f3fec46f43d7a5e9ff026f6fc436d58
Ann Kilzer <ann@curvegrid.com>
2020-06-06 19:12:45 +0900

    minimal nomination form

---
+++ web/src/components/NominateCard.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-form>
+    <v-card>
+      <v-row
+        class="px-8 py-2"
+      >
+        <v-col>
+          <v-row>
+            <v-text-field
+              label="Title"
+              outlined
+            />
+            <v-text-field
+              label="First Name"
+              outlined
+            />
+            <v-text-field
+              label="Middle Name"
+              outlined
+            />
+            <v-text-field
+              label="Last Name"
+              outlined
+            />
+          </v-row>
+          <v-text-field
+            label="Email"
+            outlined
+          />
+          <v-row>
+            <v-text-field
+              label="Job Title"
+              outlined
+            />
+            <v-text-field
+              label="Company"
+              outlined
+            />
+          </v-row>
+          <v-row>
+            <v-text-field
+              label="City"
+              outlined
+            />
+            <v-text-field
+              label="Prefecture"
+              outlined
+            />
+          </v-row>
+          <v-textarea
+            label="Speaker Bio"
+            hint="A brief description of the nominee"
+            outlined
+          />
+          <v-row>
+            <v-text-field
+              label="Submitter Name"
+              outlined
+            />
+            <v-text-field
+              label="Submitter Email"
+              outlined
+            />
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-form>
+</template>
+
+<script>
+export default {
+
+};
+</script>
--- web/src/views/NominateSpeaker.vue
+++ web/src/views/NominateSpeaker.vue
@@ -1,11 +1,24 @@
 <template>
   <v-container>
-    <h1>Nominate a speaker</h1>
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-col lg="10">
+        <h1>Nominate a Speaker</h1>
+        <nominate-card />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import NominateCard from '@/components/NominateCard.vue';
+
 export default {
+  components: {
+    NominateCard,
+  },
   data() {
     return {
 
