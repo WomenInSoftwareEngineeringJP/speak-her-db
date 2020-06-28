44524f566078b5ca51a694e35a6ec6e80537dde4
Ann 杏 Kilzer <akilzer@gmail.com>
2020-06-28 19:50:22 +0900

    remove pre-rendering

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,25 +1,5 @@
-const path = require('path');
-const PrerenderSPAPlugin = require('prerender-spa-plugin');
-
 module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
-  configureWebpack: () => {
-    if (process.env.NODE_ENV !== 'production') return null;
-    return {
-      plugins: [
-        new PrerenderSPAPlugin(
-          // Absolute path to compiled SPA
-          path.resolve(__dirname, 'dist'),
-          // List of routes to prerender
-          ['/'],
-          ['/find-speaker'],
-          {
-            // options
-          },
-        ),
-      ],
-    };
-  },
 };
