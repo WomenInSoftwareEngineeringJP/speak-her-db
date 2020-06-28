f2ec9255669e14c8cb1d42f523803e4dc91f4306
Ann 杏 Kilzer <akilzer@gmail.com>
2020-06-28 18:35:51 +0900

    pre-render it

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,5 +1,24 @@
+const path = require('path');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+
 module.exports = {
   transpileDependencies: [
     'vuetify',
   ],
+  configureWebpack: () => {
+    if (process.env.NODE_ENV !== 'production') return null;
+    return {
+      plugins: [
+        new PrerenderSPAPlugin(
+          // Absolute path to compiled SPA
+          path.resolve(__dirname, 'dist'),
+          // List of routes to prerender
+          ['/'],
+          {
+            // options
+          },
+        ),
+      ],
+    };
+  },
 };
