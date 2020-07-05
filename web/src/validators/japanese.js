43fd8980f4236cd006b9ab4c61ca6983a00a8451
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-05 23:07:50 +0900

    finish that thought

--- web/src/validators/japanese.js
+++ web/src/validators/japanese.js
@@ -1,5 +1,5 @@
 // custom vuelidate validator to ensure input is Japanese
-// inspired by
+// inspired by https://stackoverflow.com/questions/15033196/using-javascript-to-check-whether-a-string-contains-japanese-characters-includi
 const japanese = (value) => {
   const jaRegex = /^[\u3000-\u303f\u3040-\u309f\u30a0-\u30ff\uff00-\uff9f\u4e00-\u9faf\u3400-\u4dbf]*$/;
 
