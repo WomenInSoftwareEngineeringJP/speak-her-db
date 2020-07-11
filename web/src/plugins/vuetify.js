a3099824e7ee481d27558e57654e8a6c0b9d5647
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-11 19:19:21 +0900

    remove fortawesome import

--- web/src/plugins/vuetify.js
+++ web/src/plugins/vuetify.js
@@ -1,4 +1,3 @@
-import '@fortawesome/fontawesome-free/css/all.css';
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
