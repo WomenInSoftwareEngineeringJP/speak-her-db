99d8920beed8903246f1b500c3025aa007742756
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-14 04:06:43 +0900

    add component

---
+++ web/src/components/alerts/VariableAlert.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-alert
+    v-if="message !== ''"
+    :type="safeType"
+    class="my-5"
+    text
+  >
+    {{ message }}
+  </v-alert>
+</template>
+
+
+<script>
+const VALID_TYPES = new Set(['success', 'info', 'warning', 'error']);
+
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: 'info',
+    },
+  },
+  computed: {
+    safeType() {
+      if (this.type && VALID_TYPES.has(this.type)) {
+        return this.type;
+      }
+      return 'info';
+    },
+  },
+};
+</script>
