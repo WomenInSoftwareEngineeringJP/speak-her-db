feb94710bbccd5ca88f9df7e1f58e16b757a183f
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-19 19:16:31 +0900

    i18n linting!

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'plugin:vue/recommended',
     '@vue/airbnb',
+    'plugin:@intlify/vue-i18n/recommended',
   ],
   globals: {
     bus: false,
@@ -16,6 +17,13 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@intlify/vue-i18n/no-dynamic-keys': 'error',
+    '@intlify/vue-i18n/no-unused-keys': ['error', {
+      extensions: ['.js', '.vue']
+  },
+  settings: {
+    'vue-i18n': {
+      localeDir: './src/i18n/locales/*.json' // extension is glob formatting!
   },
   overrides: [
     {
