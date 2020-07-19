65afc6a6cd023624ca938cf015a47fb88a96db0e
Ann 杏 Kilzer <akilzer@gmail.com>
2020-07-19 19:29:47 +0900

    lint chill out

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -18,12 +18,15 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@intlify/vue-i18n/no-dynamic-keys': 'error',
+    '@intlify/vue-i18n/no-missing-keys': 'off',
     '@intlify/vue-i18n/no-unused-keys': ['error', {
-      extensions: ['.js', '.vue']
+      extensions: ['.js', '.vue'],
+    }],
   },
   settings: {
     'vue-i18n': {
-      localeDir: './src/i18n/locales/*.json' // extension is glob formatting!
+      localeDir: './src/i18n/locales/*.json', // extension is glob formatting!
+    },
   },
   overrides: [
     {
