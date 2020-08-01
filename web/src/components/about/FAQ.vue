b9399e8ca8e985970feabd4ca113f7549ae5acd9
Ann 杏 Kilzer <akilzer@gmail.com>
2020-08-01 14:42:03 +0900

    about page

---
+++ web/src/components/about/FAQ.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="mt-5">
+    <h2>
+      {{ $t('faq.title') }}
+    </h2>
+    <br>
+    <div
+      v-for="q in questions"
+      :key="q.question"
+    >
+      <h3>{{ q.question }}</h3>
+      <p>{{ q.answer }}</p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+export default {
+  data() {
+    return {
+      questions: [
+        {
+          question: this.$t('faq.whyQ'),
+          answer: this.$t('faq.whyA'),
+        },
+        {
+          question: this.$t('faq.obligationQ'),
+          answer: this.$t('faq.obligationA'),
+        },
+        {
+          question: this.$t('faq.freeQ'),
+          answer: this.$t('faq.freeA'),
+        },
+        {
+          question: this.$t('faq.whoQ'),
+          answer: this.$t('faq.whoA'),
+        },
+        {
+          question: this.$t('faq.correctionQ'),
+          answer: this.$t('faq.correctionA'),
+        },
+      ],
+    };
+  },
+};
+</script>
--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,11 +1,23 @@
 <template>
-  <v-container>
-    {{ $t('about.title') }}
-  </v-container>
+  <page-card>
+    <h1>{{ $t('about.title') }}</h1>
+    <br>
+    <p>{{ $t('about.summary.body') }}</p>
+    <p>{{ $t('about.purpose') }}</p>
+    <v-divider />
+    <f-a-q />
+  </page-card>
 </template>
 
 <script>
+import PageCard from '@/components/cards/PageCard.vue';
+import FAQ from '@/components/about/FAQ.vue';
+
 export default {
+  components: {
+    FAQ,
+    PageCard,
+  },
   data() {
     return {
 
