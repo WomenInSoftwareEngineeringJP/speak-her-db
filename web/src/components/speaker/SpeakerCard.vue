a8990cc0f8c5058a31bc5ad606fd78dc48265fd9
Ann 杏 Kilzer <akilzer@gmail.com>
2020-09-07 00:23:02 +0900

    secondary title / affiliation

--- web/src/components/speaker/SpeakerCard.vue
+++ web/src/components/speaker/SpeakerCard.vue
@@ -31,6 +31,9 @@
                   <v-spacer />
                   <span class="location">{{ prefecture }}</span>
                 </v-row>
+                <v-row class="my-1 mr-5">
+                  <span class="speaker-title">{{ secondTitle }}</span>
+                </v-row>
               </v-col>
             </v-expansion-panel-header>
             <v-expansion-panel-content class="px-0 mx-0">
@@ -79,6 +82,12 @@ export default {
         return `${this.speaker.get('job_title')} at ${this.speaker.get('company')}`;
       }
       return this.speaker.get('job_title') || this.speaker.get('company');
+    },
+    secondTitle() {
+      if (this.speaker.get('secondary_title') && this.speaker.get('secondary_affiliation')) {
+        return `${this.speaker.get('secondary_title')} at ${this.speaker.get('secondary_affiliation')}`;
+      }
+      return this.speaker.get('secondary_title') || this.speaker.get('secondary_affiliation');
     },
     prefecture() {
       const locationId = this.speaker.fields.location_id[0];
