114d154717d253d3dc7d7e25d26a673930de8229
Tutti Quintella <q.tuane@gmail.com>
2020-10-03 02:32:18 +0900

    [Part 2] Migrate Topics API to Netlify functions (#145)
    * add locations api as lambda function
    
    * add a service to direct requests to the lambda api
    
    * try to fetch locations using api service
    
    * fix lint
    
    * stringify responses and use the correct route
    
    * consistent return value
    
    * configs to run netlify lambda functions locally
    
    * avoid premature abstraction (make api call simple)
    
    * add topic API and function
    
    * replace topics request on nominate form
    
    * migrate and fix n+1 topics query on SpeakerCard

---
+++ web/src/lambda/topics.js
@@ -0,0 +1,59 @@
+const Airtable = require('airtable');
+
+/* eslint-disable func-names */
+exports.handler = function (event, context, callback) {
+  const response = {
+    statusCode: 200,
+    body: '',
+    headers: {
+      'content-type': 'application/json',
+      'cache-control': 'Cache-Control: max-age=60, public',
+    },
+  };
+
+  if (!process.env.AIRTABLE_API_KEY || !process.env.AIRTABLE_DB_ID) {
+    const error = 'Missing one or more Airtable ENV variables.';
+    response.statusCode = 500;
+    response.body = JSON.stringify({ error });
+
+    console.error(error);
+    return callback(error, response);
+  }
+
+  Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: process.env.AIRTABLE_API_KEY,
+  });
+
+  const base = Airtable.base(process.env.AIRTABLE_DB_ID);
+
+  if (event.httpMethod === 'GET') {
+    const query = { view: 'All', sort: [{ field: 'name', direction: 'asc' }] };
+    base('Topics')
+      .select(query)
+      .firstPage((error, records) => {
+        if (error) {
+          response.statusCode = 500;
+          response.body = JSON.stringify({ error });
+          console.error(`Error fetching Topics: ${error}`);
+          return callback(error, response);
+        }
+        response.body = JSON.stringify({ records });
+        return callback(null, response);
+      });
+  }
+  if (event.httpMethod === 'POST') {
+    base('Topics')
+      .create(event.body, { typecast: true }, (error, records) => {
+        if (error) {
+          response.statusCode = 500;
+          response.body = JSON.stringify({ error });
+          console.error(`Error fetching Topics: ${error}`);
+          return callback(error, response);
+        }
+        response.body = JSON.stringify({ records });
+        return callback(null, response);
+      });
+  }
+  return null;
+};
