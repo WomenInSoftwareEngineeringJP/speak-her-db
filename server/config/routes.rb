258ae06434df767b5b3e029d7371b7d4597527e3
Tutti Quintella <q.tuane@gmail.com>
2021-02-20 19:07:23 +0900

    implement API tests

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'application#root'
-  get 'spoken_languages', to: 'spoken_languages#index'
-  get 'topics', to: 'topics#index'
-  get 'locations', to: 'locations#index'
+
+  resources :spoken_languages, only: :index
+  resources :topics, only: :index
+  resources :locations, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
