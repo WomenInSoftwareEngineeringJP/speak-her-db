964f439408a0a367d2345d847cb252dddd1a2a6f
Tutti Quintella <q.tuane@gmail.com>
2021-02-20 15:17:28 +0900

    fix routes and add root logic

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'languages/index'
-  get 'topics/index'
-  get 'location/index'
+  root 'application#root'
+  get 'languages', to: 'languages#index'
+  get 'topics', to: 'topics#index'
+  get 'locations', to: 'locations#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
