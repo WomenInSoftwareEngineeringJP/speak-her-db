c0b65d889f5422bf9f3083e9c9196987da980e47
Ania-chan <ania.jurago@gmail.com>
2021-06-20 18:17:47 +0900

    :lipstick: Added validations for social media urls

---
+++ web/src/validators/social-media-urls.js
@@ -0,0 +1,18 @@
+import { helpers } from 'vuelidate/lib/validators';
+
+const linkedinUrl = (value) => {
+  const regex = /^https?:\/\/((www|\w\w)\.)?linkedin.com\/((in\/[^/]+\/?)|(pub\/[^/]+\/((\w|\d)+\/?){3}))$/;
+  return !helpers.req(value) || regex.test(value);
+};
+
+const facebookUrl = (value) => {
+  const regex = /^http(s*):\/\/(www.)*facebook.com\/[a-zA-Z0-9.]+$/;
+  return !helpers.req(value) || regex.test(value);
+};
+
+const twitterUrl = (value) => {
+  const regex = /^http(s*):\/\/(www.)?twitter.com\/([a-zA-Z0-9_]+)+$/;
+  return !helpers.req(value) || regex.test(value);
+};
+
+export { linkedinUrl, facebookUrl, twitterUrl };
